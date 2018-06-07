5013ac0760a202b9e7655d3ffde5ec4d44b22014
danilyanich <danilyanich@gmail.com>
2018-06-07 10:24:53 +0300

    [+] Add Ritzh method

--- 6-term/numerical-methods/2 - Методы решения граничной задачи для ОДУ-2/main.py
+++ 6-term/numerical-methods/2 - Методы решения граничной задачи для ОДУ-2/main.py
@@ -5,16 +5,17 @@ import math as ma
 import matplotlib.pyplot as plt
 
 q_x = lambda x: x
-_1_k_x = lambda x: 1 / (2 - x)
+k_x = lambda x: 2 - x
+_1_k_x = lambda x: 1 / k_x(x)
 f_x = lambda x: 2*ma.cos(x) - ma.sin(x)
 
-a, b = 0, 1
-n = 10
+_a, _b = 0, 1
 
-h = (b - a) / n
-x = [a + (i * h) for i in range(0, n + 1)]
 
-def balance():
+def balance(n):
+  h = (_b - _a) / n
+  x = [_a + (i * h) for i in range(0, n + 1)]
+
   d = np.zeros(n + 1)
   d[0] = 2/h * sp.integrate.quad(q_x, 0, h/2)[0]
   d[n] = 2/h * sp.integrate.quad(q_x, 1 - h/2, 1)[0]
@@ -57,14 +58,68 @@ def balance():
   for i in range(1, n):
     B[i] = - phi[i]
 
-  return np.linalg.solve(A, B)
+  return np.linalg.solve(A, B), x
+
+
+def ritzh(n):
+  h = (_b - _a) / n
+  x = [_a + (i * h) for i in range(0, n + 1)]
+
+  d = np.zeros(n + 1)
+
+  for i in range(1, n- 1)
+    d[i] = 1/h * sp.integrate.quad(lambda x: q_x(x)*(x - x[i - i]), x[i - 1], x[i])[0] -
+      sp.integrate.quad
+
+
+  d0= 2/h**2 * sp.integrate.quad(lambda x: q_x(x)*(h - x), 0, h)[0]
+  dN= 2/h**2 * sp.integrate.quad(lambda x: q_x(x)*(x - 1 + h), 1 - h, 1)[0]
+  f0= 2/h**2 * sp.integrate.quad(lambda x: f_x(x)*(h - x), 0, h)[0]
+  fN= 2/h**2 * sp.integrate.quad(lambda x: f_x(x)*(x - 1 + h), 1 - h, 1)[0]
+
+
+  A = np.identity(n + 1)
+
+  def Ai(xi_1, xi):
+    return 1/h * (sp.integrate.quad(lambda x: k_x(x), xi_1, xi)[0] - sp.integrate.quad(lambda x: q_x(x)*(x - xi_1)*(xi - x), xi_1, xi)[0])
+
+  def Di(xi_1,xi,xi_2):
+    return 1/h**2 * (sp.integrate.quad(lambda x: q_x(x)*(x - xi_1), xi_1, xi)[0] - sp.integrate.quad(lambda x: q_x(x)*(xi - x), xi, xi_2)[0])
+
+  A[0, 0] = -Ai(0, h)/h - (1 + h/2 * d0)
+  A[0, 1] = Ai(0, h)/h
+
+  A[n, n - 1] = Ai(1 - h, 1)/h
+  A[n, n] = -Ai(1 - h, 1)/h - (ma.tan(1) + h/2 * dN)
+
+  for i in range(1,n):
+    A[i, i + 1] = Ai(x[i], x[i+1])/h**2
+    A[i, i] = -((Ai(x[i], x[i+1]) + Ai(x[i-1], x[i]))/h**2 + Di(x[i - 1], x[i], x[i + 1]))
+    A[i,i - 1] = Ai(x[i - 1], x[i])/h**2
+
+
+  B = np.zeros(n + 1)
+
+  def fi(xi_1, xi, xi_2):
+    return 1/h**2 * (sp.integrate.quad(lambda x: f_x(x)*(x - xi_1), xi_1, xi)[0] - sp.integrate.quad(lambda x: f_x(x)*(xi-x), xi, xi_2)[0])
+
+  B[0] = -(1 + h/2*f0)
+  B[n] = -h/2*fN
+
+  for i in range(1,n):
+    B[i] = -fi(x[i - 1], x[i], x[i + 1])
+
+  return np.linalg.solve(A, B), x
 
 
 if __name__ == "__main__":
-  solutionBalance = balance()
 
-  print('Баланс:')
-  print(solutionBalance)
+  for n in range(9, 10):
+    solutionBalance, x = balance(10 * n)
+    plt.plot(x, solutionBalance)
 
-  plt.plot(x, solutionBalance)
+  for n in range(9, 10):
+    solutionRitzh, x = ritzh(10 * n)
+    plt.plot(x, solutionRitzh)
+
   plt.show()
