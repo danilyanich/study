25443e02b9dec8a8c61dc7f67f682d7c9ee544da
danilyanich <danilyanich@gmail.com>
2018-06-07 13:03:34 +0300

    [+]

--- 6-term/numerical-methods/1 - Разностные схемы для ОДУ-2/main.py
+++ 6-term/numerical-methods/1 - Разностные схемы для ОДУ-2/main.py
@@ -3,14 +3,13 @@ import math as ma
 import matplotlib.pyplot as plt
 
 f = lambda x: 1 / ma.cos(x)**2
-a, b = 0, 0.5
-n = 10
-
-h = (b - a) / n
-x = [a + (i * h) for i in range(0, n + 1)]
+_a, _b = 0, 0.5
 
 
-def main():
+def solve(n):
+  h = (_b - _a) / n
+  x = [_a + (i * h) for i in range(0, n + 1)]
+
   A = np.identity(n + 1)
   A[0, 0] = -1 / h
   A[0, 1] = 1 / h
@@ -29,20 +28,52 @@ def main():
   for i in range(1, n - 1):
     B[i] = 1 / ma.cos(x[i]) ** 2
 
-  return A, B
+  return np.linalg.solve(A, B), x
+
+
+def solve_2(n):
+  h = (_b - _a) / n
+  x = [_a + (i * h) for i in range(0, n + 1)]
+
+  al_0 = -2*h / (2 - h) + 1
+  be_0 = 2 / (2 - h)
+  ga_0 = h / (2 - h)
+
+  al_1 = h**2 / ma.cos(0.5)**2
+  be_1 = -h
+  ga_1 = -ma.cos(0.5)**2 + h**2 / (2*ma.cos(0.5)**2)
+
+  A = np.identity(n + 1)
+  A[0, 0] = al_0 - 1/h * be_0
+  A[0, 1] = 1/h * be_0
+
+  A[n, n - 1] = -1/h * be_1
+  A[n, n] = al_1 + 1/h * be_1
+
+  for i in range(1, n):
+    A[i, i - 1] = 1 / h**2
+    A[i, i] = -(2 / h**2 + 1 / h + 2 / ma.cos(x[i]) ** 2)
+    A[i, i + 1] = 1 / h**2 + 1 / h
+
+  B = np.zeros(n + 1)
+  B[0] = ga_0
+  B[n] = ga_1
+
+  for i in range(1, n):
+    B[i] = 1 / ma.cos(x[i]) ** 2
+
+  return np.linalg.solve(A, B), x
 
 
 
 if __name__ == "__main__":
-  A, B =
-  y = np.linalg.solve(A, B)
+  for n in range(1, 10):
+    solution, x = solve(10 * n)
+    plt.plot(x, solution)
 
-  plt.plot(x, y)
+
+  for n in range(999, 1000):
+    solution_2, x = solve_2(10 * n)
+    plt.plot(x, solution_2)
+
   plt.show()
-
-  print(y, x)
-
-  for a in A:
-    for e in a:
-      print("{:10.2f}".format(e), end="")
-    print()
--- 6-term/numerical-methods/2 - Методы решения граничной задачи для ОДУ-2/main.py
+++ 6-term/numerical-methods/2 - Методы решения граничной задачи для ОДУ-2/main.py
@@ -11,31 +11,34 @@ f_x = lambda x: 2*ma.cos(x) - ma.sin(x)
 
 _a, _b = 0, 1
 
+def integrate(func, _from, to):
+  return sp.integrate.quad(func, _from, to)[0]
+
 
 def balance(n):
   h = (_b - _a) / n
   x = [_a + (i * h) for i in range(0, n + 1)]
 
   d = np.zeros(n + 1)
-  d[0] = 2/h * sp.integrate.quad(q_x, 0, h/2)[0]
-  d[n] = 2/h * sp.integrate.quad(q_x, 1 - h/2, 1)[0]
+  d[0] = 2/h * integrate(q_x, 0, h/2)
+  d[n] = 2/h * integrate(q_x, 1 - h/2, 1)
 
   for i in range(1, n):
-    d[i] = 1/h * sp.integrate.quad(q_x, x[i] - h/2, x[i] + h/2)[0]
+    d[i] = 1/h * integrate(q_x, x[i] - h/2, x[i] + h/2)
 
 
   a = np.zeros(n + 1)
 
   for i in range(1, n + 1):
-    a[i] = 1 / (1/h * sp.integrate.quad(_1_k_x, x[i - 1], x[i])[0])
+    a[i] = 1 / (1/h * integrate(_1_k_x, x[i - 1], x[i]))
 
 
   phi = np.zeros(n + 1)
-  phi[0] = 2/h * sp.integrate.quad(f_x, 0, h/2)[0]
-  phi[n] = 2/h * sp.integrate.quad(f_x, 1 - h/2, 1)[0]
+  phi[0] = 2/h * integrate(f_x, 0, h/2)
+  phi[n] = 2/h * integrate(f_x, 1 - h/2, 1)
 
   for i in range(1, n):
-    phi[i] = 1/h * sp.integrate.quad(f_x, x[i] - h/2, x[i] + h/2)[0]
+    phi[i] = 1/h * integrate(f_x, x[i] - h/2, x[i] + h/2)
 
 
   A = np.identity(n + 1)
@@ -65,60 +68,86 @@ def ritzh(n):
   h = (_b - _a) / n
   x = [_a + (i * h) for i in range(0, n + 1)]
 
-  d = np.zeros(n + 1)
+  # d = np.zeros(n + 1)
 
-  for i in range(1, n- 1)
-    d[i] = 1/h * sp.integrate.quad(lambda x: q_x(x)*(x - x[i - i]), x[i - 1], x[i])[0] -
-      sp.integrate.quad
+  # for i in range(1, n- 1)
+  #   d[i] = 1/h * sp.integrate.quad(lambda x: q_x(x)*(x - x[i - i]), x[i - 1], x[i])[0] -
+  #     sp.integrate.quad(lambda x: )
 
 
-  d0= 2/h**2 * sp.integrate.quad(lambda x: q_x(x)*(h - x), 0, h)[0]
-  dN= 2/h**2 * sp.integrate.quad(lambda x: q_x(x)*(x - 1 + h), 1 - h, 1)[0]
-  f0= 2/h**2 * sp.integrate.quad(lambda x: f_x(x)*(h - x), 0, h)[0]
-  fN= 2/h**2 * sp.integrate.quad(lambda x: f_x(x)*(x - 1 + h), 1 - h, 1)[0]
+  # d0= 2/h**2 * sp.integrate.quad(lambda x: q_x(x)*(h - x), 0, h)[0]
+  # dN= 2/h**2 * sp.integrate.quad(lambda x: q_x(x)*(x - 1 + h), 1 - h, 1)[0]
+  # f0= 2/h**2 * sp.integrate.quad(lambda x: f_x(x)*(h - x), 0, h)[0]
+  # fN= 2/h**2 * sp.integrate.quad(lambda x: f_x(x)*(x - 1 + h), 1 - h, 1)[0]
 
 
   A = np.identity(n + 1)
 
-  def Ai(xi_1, xi):
-    return 1/h * (sp.integrate.quad(lambda x: k_x(x), xi_1, xi)[0] - sp.integrate.quad(lambda x: q_x(x)*(x - xi_1)*(xi - x), xi_1, xi)[0])
+  A[0, 0] = 1/h**2 * (integrate(k_x, 0, h) + integrate(lambda _x: q_x(_x) * (_x - h)**2, 0, h)) +  1
+  A[n, n] = 1/h**2 * (integrate(k_x, 1 - h, 1) + integrate(lambda _x: q_x(_x) * (_x - 1 + h)**2, 1 - h, 1)) + ma.tan(1)
 
-  def Di(xi_1,xi,xi_2):
-    return 1/h**2 * (sp.integrate.quad(lambda x: q_x(x)*(x - xi_1), xi_1, xi)[0] - sp.integrate.quad(lambda x: q_x(x)*(xi - x), xi, xi_2)[0])
+  for i in range(1, n):
+    res = 1/h**2 * (integrate(lambda _x: q_x(_x) * (x[i + 1] - _x) * (_x - x[i]), x[i], x[i + 1]) - integrate(k_x, x[i], x[i + 1]))
+    A[i, i + 1] = res
+    A[i + 1, i] = res
 
-  A[0, 0] = -Ai(0, h)/h - (1 + h/2 * d0)
-  A[0, 1] = Ai(0, h)/h
+  for i in range(1, n):
+    A[i, i] = 1/h**2 * (integrate(k_x, x[i - 1], x[i + 1]) + integrate(lambda _x: q_x(_x)*(_x - x[i - 1])**2, x[i - 1], x[i]) + integrate(lambda _x: q_x(_x)*(x[i + 1] - _x)**2, x[i], x[i + 1]))
 
-  A[n, n - 1] = Ai(1 - h, 1)/h
-  A[n, n] = -Ai(1 - h, 1)/h - (ma.tan(1) + h/2 * dN)
 
-  for i in range(1,n):
-    A[i, i + 1] = Ai(x[i], x[i+1])/h**2
-    A[i, i] = -((Ai(x[i], x[i+1]) + Ai(x[i-1], x[i]))/h**2 + Di(x[i - 1], x[i], x[i + 1]))
-    A[i,i - 1] = Ai(x[i - 1], x[i])/h**2
+  # def Ai(xi_1, xi):
+  #   return 1/h * (sp.integrate.quad(lambda x: k_x(x), xi_1, xi)[0] - sp.integrate.quad(lambda x: q_x(x)*(x - xi_1)*(xi - x), xi_1, xi)[0])
+
+  # def Di(xi_1,xi,xi_2):
+  #   return 1/h**2 * (sp.integrate.quad(lambda x: q_x(x)*(x - xi_1), xi_1, xi)[0] - sp.integrate.quad(lambda x: q_x(x)*(xi - x), xi, xi_2)[0])
+
+  # A[0, 0] = -Ai(0, h)/h - (1 + h/2 * d0)
+  # A[0, 1] = Ai(0, h)/h
+
+  # A[n, n - 1] = Ai(1 - h, 1)/h
+  # A[n, n] = -Ai(1 - h, 1)/h - (ma.tan(1) + h/2 * dN)
+
+  # for i in range(1,n):
+  #   A[i, i + 1] = Ai(x[i], x[i+1])/h**2
+  #   A[i, i] = -((Ai(x[i], x[i+1]) + Ai(x[i-1], x[i]))/h**2 + Di(x[i - 1], x[i], x[i + 1]))
+  #   A[i,i - 1] = Ai(x[i - 1], x[i])/h**2
 
 
   B = np.zeros(n + 1)
 
-  def fi(xi_1, xi, xi_2):
-    return 1/h**2 * (sp.integrate.quad(lambda x: f_x(x)*(x - xi_1), xi_1, xi)[0] - sp.integrate.quad(lambda x: f_x(x)*(xi-x), xi, xi_2)[0])
+  B[0] = 1/h * integrate(lambda _x: f_x(_x) * (_x - h), 0, h) + 1
+  B[n] = 1/h * integrate(lambda _x: f_x(_x) * (_x - 1 + h), 1 - h, 1)
 
-  B[0] = -(1 + h/2*f0)
-  B[n] = -h/2*fN
+  for i in range(1, n):
+    B[i] = 1/h * (integrate(lambda _x: f_x(_x) * (_x - x[i - 1]), x[i - 1], x[i]) + integrate(lambda _x: f_x(_x) * (x[i+1] - _x), x[i], x[i + 1]))
 
-  for i in range(1,n):
-    B[i] = -fi(x[i - 1], x[i], x[i + 1])
+
+  # def fi(xi_1, xi, xi_2):
+  #   return 1/h**2 * (sp.integrate.quad(lambda x: f_x(x)*(x - xi_1), xi_1, xi)[0] - sp.integrate.quad(lambda x: f_x(x)*(xi-x), xi, xi_2)[0])
+
+  # B[0] = -(1 + h/2*f0)
+  # B[n] = -h/2*fN
+
+  # for i in range(1,n):
+  #   B[i] = -fi(x[i - 1], x[i], x[i + 1])
+
+  for r in A:
+    for e in r:
+      print('{:15.5}'.format(e), end='')
+    print()
+
+  print(B)
 
   return np.linalg.solve(A, B), x
 
 
 if __name__ == "__main__":
 
-  for n in range(9, 10):
+  for n in range(1, 2):
     solutionBalance, x = balance(10 * n)
     plt.plot(x, solutionBalance)
 
-  for n in range(9, 10):
+  for n in range(1, 10):
     solutionRitzh, x = ritzh(10 * n)
     plt.plot(x, solutionRitzh)
 
